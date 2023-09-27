5b79353acb281a0be7e3b17323717e7a730d985d
fancy45daddy <124528204+fancy45daddy@users.noreply.github.com>
2023-09-26 21:47:13 -0700

    Update fairgroundClickersptr.py

--- fairgroundClickersptr.py
+++ fairgroundClickersptr.py
@@ -1,4 +1,4 @@
-import aiohttp, asyncio, bs4, fake_useragent, argparse, re, builtins, locale, sys, numpy, cv2
+import aiohttp, asyncio, bs4, fake_useragent, argparse, re, builtins, locale, sys
 parser = argparse.ArgumentParser()
 parser.add_argument('password')
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
